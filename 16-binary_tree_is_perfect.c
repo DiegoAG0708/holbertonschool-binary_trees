812dafee8f6c058bbfee9a95921bf1d33c6f8994
root <root@ip-10-42-127-194.ec2.internal>
2025-12-02 17:39:06 +0000

    Task 16: self-contained perfect check using height equality and full structure; Betty-compliant

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,27 +1,67 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_is_perfect - Checks if a binary tree is perfect
-* @tree: Pointer to the root node of the tree to check
+* bt_height - Measures height in edges (leaf = 0)
+* @tree: Pointer to the root node of the tree to measure
 *
-* Return: 1 if perfect, 0 otherwise
+* Return: Height in edges, or 0 if tree is NULL
 */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+static size_t bt_height(const binary_tree_t *tree)
 {
-size_t left_size;
-size_t right_size;
+size_t lh;
+size_t rh;
 
 if (tree == NULL)
 return (0);
 
-left_size = binary_tree_size(tree->left);
-right_size = binary_tree_size(tree->right);
+if (tree->left == NULL && tree->right == NULL)
+return (0);
 
-if (left_size == right_size)
+lh = bt_height(tree->left);
+rh = bt_height(tree->right);
+
+return ((lh > rh ? lh : rh)+1);
+}
+
+/**
+* is_perfect - Recursively checks if a tree is perfect
+* @tree: Pointer to current node
+*
+* Return: 1 if perfect, 0 otherwise
+*/
+static int is_perfect(const binary_tree_t *tree)
 {
+size_t lh;
+size_t rh;
+
+if (tree == NULL)
+return (1); /* empty subtree is considered perfect structurally */
+
 if (tree->left == NULL && tree->right == NULL)
 return (1);
-return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
-}
+
+if (tree->left == NULL || tree->right == NULL)
 return (0);
+
+lh = bt_height(tree->left);
+rh = bt_height(tree->right);
+
+if (lh != rh)
+return (0);
+
+return (is_perfect(tree->left) && is_perfect(tree->right));
+}
+
+/**
+* binary_tree_is_perfect - Checks if a binary tree is perfect
+* @tree: Pointer to the root node of the tree to check
+*
+* Return: 1 if perfect, 0 otherwise; if tree is NULL, return 0
+*/
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (0);
+
+return (is_perfect(tree));
 }
