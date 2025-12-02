69cab2e6af04d697fc7d3adfcb0ba4e355cec934
root <root@ip-10-42-127-194.ec2.internal>
2025-12-02 17:30:44 +0000

    Task 14: Make balance self-contained with internal height helper; Betty-compliant

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,31 @@
 #include "binary_trees.h"
 
 /**
+* bt_height - Measures the height of a binary tree (internal helper)
+* @tree: Pointer to the root node of the tree to measure
+*
+* Return: Height of the tree, or 0 if tree is NULL
+*/
+static size_t bt_height(const binary_tree_t *tree)
+{
+size_t left_height;
+size_t right_height;
+
+if (tree == NULL)
+return (0);
+
+if (tree->left == NULL && tree->right == NULL)
+return (0);
+
+left_height = bt_height(tree->left);
+right_height = bt_height(tree->right);
+
+if (left_height > right_height)
+return (left_height + 1);
+return (right_height + 1);
+}
+
+/**
 * binary_tree_balance - Measures the balance factor of a binary tree
 * @tree: Pointer to the root node of the tree to measure
 *
@@ -14,8 +39,8 @@ int right_height;
 if (tree == NULL)
 return (0);
 
-left_height = (int)binary_tree_height(tree->left);
-right_height = (int)binary_tree_height(tree->right);
+left_height = (int)bt_height(tree->left);
+right_height = (int)bt_height(tree->right);
 
 return (left_height - right_height);
 }
